02d1a577566b99fbf75f293d42b5a8959a70725c
Sandip Nakrnai <sandip.nakrani@karsun-llc.com>
2019-02-21 13:41:07 -0500

    added error

--- data_capture/management/commands/initgroups.py
+++ data_capture/management/commands/initgroups.py
@@ -63,7 +63,7 @@ class Command(BaseCommand):
             group.permissions = get_permissions_from_ns_codenames(perms)
             group.save()
         except Exception as e:
-            self.stderr.write("Error stroing group. " + e)
+            self.stderr.write(f"Error stroing group  {e}.")
             print(e)
 
     @transaction.atomic
