02e421e26836edf2bbe73344f21f51fef9ae1283
kjsarabi <kevin.sarabi@gsa.gov>
2019-06-14 21:22:37 -0400

    PSHCDT-87:CALC's Session Timeout updated

--- data_explorer/tests.py
+++ data_explorer/tests.py
@@ -7,9 +7,8 @@ class DataExplorerTests(TestCase):
     def test_logout_works(self):
         with mock.patch('django.contrib.auth.logout') as logout_mock:
             response = self.client.get('/logout/')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 302)
             self.assertEqual(logout_mock.call_count, 1)
-            self.assertEqual(response.status_code, 200)
 
     def test_dap_is_part_of_unauthenticated_requests(self):
         response = self.client.get('/')
