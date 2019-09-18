184d0669941c6f07a22cbc9ab071d30c35708842
Sandip Nakrani <s.nakrani@gmail.com>
2019-09-18 13:48:24 -0400

    added type to dictionary

--- data_capture/analysis/core.py
+++ data_capture/analysis/core.py
@@ -222,7 +222,7 @@ def analyze_gleaned_data(gleaned_data):
             )
             price_list.save()
             gleaned_data.add_to_price_list(price_list)
-            cached_rows = {}
+            cached_rows: Dict[str, str] = {}
             for row in price_list.rows.all():
                 key =  row.labor_category + row.min_years_experience + row.education_level
                 if key in cached_rows:
