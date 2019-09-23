eb94a2117bd3c98ced49862cea9591d53a7a54af
Sandip Nakrani <s.nakrani@gmail.com>
2019-09-23 14:50:48 -0400

    removed commented test

--- slackbot/tests.py
+++ slackbot/tests.py
@@ -1,42 +1,12 @@
 from unittest.mock import patch
 from django.test import TestCase, override_settings
 from click.testing import CliRunner
-import httmock
+# import httmock
 
 from . import signals
-from . import bot
+# from . import bot
 from .management.commands import sendtestslack
 from data_capture.models import SubmittedPriceList
-
-
-# class BotTests(TestCase):
-    # @override_settings(SLACKBOT_WEBHOOK_URL='')
-    # @patch.object(bot.logger, 'debug')
-    # def test_sendmsg_returns_false_when_settings_are_not_defined(self, m):
-    #     self.assertFalse(bot.sendmsg('hi'))
-    #     m.assert_called_with(
-    #         'SLACKBOT_WEBHOOK_URL is empty; not sending message.')
-
-    # @override_settings(SLACKBOT_WEBHOOK_URL='http://boop')
-    # @patch.object(bot.logger, 'exception')
-    # def test_sendmsg_returns_false_when_post_to_webhook_fails(self, m):
-    #     def mock_500_response(url, request):
-    #         self.assertEqual(url.netloc, 'boop')
-    #         return httmock.response(500)
-
-    #     with httmock.HTTMock(mock_500_response):
-    #         self.assertFalse(bot.sendmsg('hi'))
-
-    #     m.assert_called_with('Error occurred when sending Slack message.')
-
-    # @override_settings(SLACKBOT_WEBHOOK_URL='http://boop')
-    # def test_sendmsg_returns_true_on_success(self):
-    #     def mock_200_response(url, request):
-    #         self.assertEqual(url.netloc, 'boop')
-    #         return httmock.response(200)
-
-    #     with httmock.HTTMock(mock_200_response):
-    #         self.assertTrue(bot.sendmsg('hi'))
 
 
 class SendtestslackTests(TestCase):
