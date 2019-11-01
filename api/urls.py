04d4f39e48b2222ab8acb6558417b3061556c27f
Sandip Nakrani <s.nakrani@gmail.com>
2019-11-01 14:13:02 -0400

    Build Error fix

--- api/urls.py
+++ api/urls.py
@@ -9,13 +9,13 @@ from .capability_statement import capability_statement
 from api import views
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="CALC API",
-      default_version='v2',
-      description="CALC API DOCUMENTATION",
-      contact=openapi.Contact(email="calc@gsa.gov"),
-   ),
-   public=True,
+    openapi.Info(
+        title="CALC API",
+        default_version='v2',
+        description="CALC API DOCUMENTATION",
+        contact=openapi.Contact(email="calc@gsa.gov"),
+    ),
+    public=True,
 )
 
 urlpatterns = [
@@ -25,7 +25,8 @@ urlpatterns = [
     url(r'^schedules/$', views.ScheduleMetadataList.as_view()),
     url(r'^docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^$', RedirectView.as_view(pattern_name='api-docs:docs-index')),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', 
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^contract/capability_statement/(?P<contractnumber>[\w-]+)/$',
         capability_statement.get_capability_statment),  # after login
     url(r'^contract/capability_statement/url/(?P<contractnumberlist>[\w,-]+)/$',
