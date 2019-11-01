8a3b3df97bd279f913a544a9bf024746452e6cc4
dsomalaraju <dinesh.somalaraju@gmail.com>
2019-11-01 13:31:37 -0400

    UI changes

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from rest_framework.documentation import include_docs_urls
 
-from .capability_statement import get_capability_statement
+from .capability_statement import capability_statement
 
 from api import views
 
@@ -16,8 +16,8 @@ urlpatterns = [
         description=views.DOCS_DESCRIPTION,
     )),
     url(r'^$', RedirectView.as_view(pattern_name='api-docs:docs-index')),
-    url(r'^capability_statement/contract_number=(?P<contractnumber>[\w-]+)/$',
-        get_capability_statement.get_capability_statment),  # after login
-    url(r'^capability_statement/contract_number_list=(?P<contractnumberlist>[\w,-]+)/$',
-        get_capability_statement.get_bulk_capability_statment),
+    url(r'^contract/capability_statement/(?P<contractnumber>[\w-]+)/$',
+        capability_statement.get_capability_statment),  # after login
+    url(r'^contract/capability_statement/url/(?P<contractnumberlist>[\w,-]+)/$',
+        capability_statement.get_bulk_capability_statment),
 ]
