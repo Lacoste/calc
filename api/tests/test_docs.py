fb83b1191a5d3a508102bcf0a6388c09a4eb0dbc
Sandip Nakrani <s.nakrani@gmail.com>
2019-11-01 16:54:49 -0400

    removed extra line

--- api/tests/test_docs.py
+++ api/tests/test_docs.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 
 class DocsTests(TestCase):
     def test_docs_do_not_explode(self):
-        res = self.client.get('/api/')
+        res = self.client.get('/api/docs/')
         self.assertEqual(res.status_code, 200)
