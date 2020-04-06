56e79ef9c36c5a55b0952ff8f350f72ed366fad2
dinesh somalaraju <dinesh.somalaraju@gmail.com>
2020-04-05 22:14:16 -0400

    checking API failure rates

--- frontend/source/js/data-explorer/api.js
+++ frontend/source/js/data-explorer/api.js
@@ -3,10 +3,10 @@ import xhr from 'xhr';
 import * as qs from 'querystring';
 
 export const API_BASE = '/api';
-export const API_RATES_SCHEDULES = '/schedules';
-export const API_PATH_RATES_CSV = '/rates/csv';
-export const API_PATH_RATES = '/rates';
-export const API_PATH_SEARCH = '/search'; 
+export const API_RATES_SCHEDULES = 'schedules';
+export const API_PATH_RATES_CSV = 'rates/csv';
+export const API_PATH_RATES = 'rates';
+export const API_PATH_SEARCH = 'search'; 
 
 /** 
  * @typedef {Object} ScheduleMetadata
