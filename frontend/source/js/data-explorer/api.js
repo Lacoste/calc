041605316c97748e97c1c1229abe27fef3d71d21
dinesh somalaraju <dinesh.somalaraju@gmail.com>
2020-04-05 21:39:54 -0400

    checking print statements in dev

--- frontend/source/js/data-explorer/api.js
+++ frontend/source/js/data-explorer/api.js
@@ -6,7 +6,7 @@ export const API_BASE = '/api';
 export const API_RATES_SCHEDULES = '/schedules';
 export const API_PATH_RATES_CSV = '/rates/csv';
 export const API_PATH_RATES = '/rates';
-export const API_PATH_SEARCH = '/search';
+export const API_PATH_SEARCH = '/search'; 
 
 /** 
  * @typedef {Object} ScheduleMetadata
@@ -21,7 +21,12 @@ export default class API {
   }
 
   get({ uri, data }, callback) {
+    alert('this.basePath');
+    alert(this.basePath);
     let path = this.basePath + uri;
+    alert('path');
+    alert(path);
+
     if (data) {
       path += `?${qs.stringify(data)}`;
     }
--- frontend/source/js/data-explorer/rates-request.js
+++ frontend/source/js/data-explorer/rates-request.js
@@ -56,7 +56,7 @@ export default class StoreRatesAutoRequester {
     };
 
     store.dispatch(startRatesRequest());
-
+    alert(API_PATH_RATES);
     this.request = this.api.get({
       uri: API_PATH_RATES,
       data: Object.assign(defaults, data),
