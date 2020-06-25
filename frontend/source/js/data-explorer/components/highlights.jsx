dc438af0d8e7b6dacc44e31e1a88cb84233ef0d6
dinesh somalaraju <dinesh.somalaraju@gsa.gov>
2020-06-25 12:30:03 -0400

    Adding median to the graph

--- frontend/source/js/data-explorer/components/highlights.jsx
+++ frontend/source/js/data-explorer/components/highlights.jsx
@@ -4,15 +4,16 @@ import { connect } from 'react-redux';
 
 import {
   formatPrice,
+  getMedian
 } from '../util';
 
 export function Highlights({
   stdDeviation,
   avgPrice,
+  current_prices,
 }) {
   const stdDevMinus = avgPrice - stdDeviation;
   const stdDevPlus = avgPrice + stdDeviation;
-
   // TODO: The original implementation faded the proposed price in and
   // out as it was set/unset. We might want to do the same thing here.
 
@@ -37,6 +38,14 @@ Average price
             {formatPrice(avgPrice)}
           </h5>
         </div>
+        <div className="medianblock standard-deviation-block">
+          <h5 className="standard-deviation-title">
+          Median Price
+          </h5>
+          <h5 className="sd-highlight">
+              ${formatPrice(getMedian(current_prices))}
+          </h5>
+        </div>
         <div className="standard-deviation-block">
           <h5 className="standard-deviation-title">
 Std deviation +1
@@ -54,12 +63,14 @@ Std deviation +1
 Highlights.propTypes = {
   stdDeviation: PropTypes.number.isRequired,
   avgPrice: PropTypes.number.isRequired,
+  current_prices: PropTypes.any.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     stdDeviation: state.rates.data.first_standard_deviation,
     avgPrice: state.rates.data.average,
+    current_prices:state.rates.data.results.map((n) => n.current_price)
   };
 }
 
